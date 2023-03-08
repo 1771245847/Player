f3b7cd13b126470e56071aa2da07489c52b1116c
1771245847 <55345096+1771245847@users.noreply.github.com>
2023-03-08 19:35:45 +0800

    2023-3-8

---
+++ admin/data.php
@@ -0,0 +1,43 @@
+<?php
+ $yzm =  array (
+  'danmuon' => 'on',
+  'next' => 'on',
+  'color' => '#00a1d6',
+  'logo' => '',
+  'trytime' => '3',
+  'yjbt' => '残月解析-弹幕播放器',
+  'yjurl' => '#',
+  'waittime' => '3',
+  'sendtime' => '3',
+  'av' => '',
+  'mylink' => '',
+  'api' => '/player/dmku/',
+  'dmrule' => '',
+  'pbgjz' => '残月解析-弹幕播放器',
+  'ads' => 
+  array (
+    'set' => 
+    array (
+      'state' => '2',
+      'group' => 'null',
+      'pic' => 
+      array (
+        'time' => '',
+        'img' => '',
+        'link' => '',
+      ),
+      'vod' => 
+      array (
+        'url' => '',
+        'link' => '',
+      ),
+    ),
+    'pause' => 
+    array (
+      'pic' => '',
+      'link' => '',
+    ),
+  ),
+  'adminpass' => '123456',
+);
+?>
